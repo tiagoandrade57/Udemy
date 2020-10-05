83e0d0fdba7fbecd39deee6eda28cc6d29c1ec66
Tiago Andrade <tsadevapps@gmail.com>
2020-10-05 15:25:55 -0300

    teste

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemadePedidosdeProdutos.Startup))]
+namespace SistemadePedidosdeProdutos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
